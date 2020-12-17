4355c36871aaf09b863bb2a478c78b252e3833bb
Bart Leers <r0780104@student.vives.be>
2020-12-17 14:38:45 +0100

    aanpassing footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -6,7 +6,7 @@
                                                                                         class="figure-img rounded"
                                                                                         alt="anti spam"
                                                                                         style="width:35%"></a></div>
-                <p><i class="far fa-edit"></i> 16/12/2020</p>
+                <p><i class="far fa-edit"></i> 17/12/2020</p>
                 <div class="col-auto">
                     <address>
                         <h5>Bart Leers <i class="far fa-copyright"></i> 2021 </h5>

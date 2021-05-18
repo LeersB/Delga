eb8794c73a7c2edb5f8e145a93464ea640b4484e
Bart Leers <r0780104@student.vives.be>
2021-05-18 15:51:43 +0200

    aanpassing logout.php zorgen dat indien al reeds uitgelogd bent eveneens terug op index.php komt

--- logout.php
+++ logout.php
@@ -1,16 +1,21 @@
 <?php
 session_start();
-if (isset($_SESSION['loggedin'])) {
-    unset($_COOKIE['rememberme']);
-    setcookie('rememberme', '', time() - 3600);
-    unset($_SESSION['loggedin']);
-    unset($_SESSION['voornaam']);
-    unset($_SESSION['achternaam']);
-    unset($_SESSION['user_id']);
-    unset($_SESSION['user_level']);
-    session_destroy();
-
-    header('Location: index.php');
+if (!isset($_SESSION['loggedin'])) {
+    header("location: index.php");
     exit();
+} else {
+    if (isset($_SESSION['loggedin'])) {
+        unset($_COOKIE['rememberme']);
+        setcookie('rememberme', '', time() - 3600);
+        unset($_SESSION['loggedin']);
+        unset($_SESSION['voornaam']);
+        unset($_SESSION['achternaam']);
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_level']);
+        session_destroy();
+
+        header('Location: index.php');
+        exit();
+    }
 }
 ?>

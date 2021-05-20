c566aff75dd30932c6dda325111b83ef2c4dec85
Bart Leers <r0780104@student.vives.be>
2021-05-20 17:35:41 +0200

    aanpassing info

--- privacy.php
+++ privacy.php
@@ -36,31 +36,29 @@ include 'main.php';
 
                 <p>Je privacy is belangrijk voor ons.</p>
 
-                <p> Daarom verwerken we alle persoonsgegevens die wij van je verkrijgen als klant of als bezoeker van
+                <p> Daarom verwerken we alle persoonsgegevens die wij van U verkrijgen als klant of als bezoeker van
                     onze website alleen op een manier die in overeenstemming is met de toepasselijke
                     regelgeving inzake gegevensbescherming,
                     <br>in het bijzonder de Algemene Verordening Gegevensbescherming 2016/679 van 27 april 2016
                     ("GDPR").</p>
 
-                <p>Wij willen u door middel van deze communicatie informeren over de verzameling en verwerking van
+                <p>Wij willen U door middel van deze communicatie informeren over de verzameling en verwerking van
                     uw persoonsgegevens.
-                    <br>Uw persoonsgegevens worden door Onderhoudsproducten Delga bewaard en verwerk in functie
+                    <br>Uw persoonsgegevens worden door Onderhoudsproducten Delga bewaard en verwerkt in functie
                     van enkel commerciële belangen.</p>
 
-                <p>U hoeft niets te ondernemen indien u verklaart kennis te hebben genomen van de inhoud van deze
-                    communicatie<br>en u zicht onvoorwaardelijk akkoord verklaart met de verwerking van uw
+                <p>U hoeft niets te ondernemen indien U verklaart kennis te hebben genomen van de inhoud van deze
+                    communicatie en<br> U zich onvoorwaardelijk akkoord verklaart met de verwerking van uw
                     persoonsgegevens conform de bepalingen binnen de GDPR-wetgeving.
-                    U kan steeds onze verkoopsvoorwaarden ontvangen, gelieve ons hiervoor te contacteren.
-                    <br>Indien u toch bezwaar maakt tegen deze verwerking van uw persoonsgegevens,
-                    <br>kan u zich richten tot "Onderhoudsproducten Delga" per e-mail naar <a
-                            href="mailto:info@delga.be?subject=info" class="text-primary">info@delga.be</a>.</p>
+                    <br>U kan steeds onze verkoopsvoorwaarden ontvangen, gelieve ons hiervoor te contacteren.
+                    <br>Indien U toch bezwaar maakt tegen deze verwerking van uw persoonsgegevens,
+                    <br>kan U zich richten tot "Onderhoudsproducten Delga" per e-mail naar
+                    <a href="mailto:info@delga.be?subject=info" class="text-primary">info@delga.be</a>.</p>
 
-                <p>Verder willen wij u er op wijzen dat u het recht heeft om ten allen tijde uw voorkeuren aan te
+                <p>Verder willen wij U er op wijzen dat U het recht heeft om ten allen tijde uw voorkeuren aan te
                     passen,
                     <br>gelieve ons hiervan op de hoogte te brengen via bovenvermelde e-mail communicatie.
-                    <br>Wij stellen alles in het werk om ervoor te zorgen dat u uw persoonsgegevens in alle veiligheid
-                    aan
-                    ons kan blijven toevertrouwen. </p>
+                </p>
 
                 <p>Dank voor jullie vertrouwen.</p>
                 <p>Delhaye Gabriël<br>Zaakvoerder</p>

62ae74545ffffd22ebd0e25a77d6f989c57b088a
Bart Leers <r0780104@student.vives.be>
2021-06-16 10:00:42 +0200

    aanpassingen header

--- includes/header.php
+++ includes/header.php
@@ -85,8 +85,8 @@
             ?>
         </ul>
 
-        <form class="form-inline mt-2 mt-sm-2" action="zoeken.php" method="get">
-            <div class="input-group mr-sm-2">
+        <form class="d-flex mt-2" action="zoeken.php" method="get">
+            <div class="input-group mr-2">
                 <input type="text" class="form-control" name="query" placeholder="Product zoeken"
                        aria-label="Product zoeken"
                        value="<?= isset($_GET['query']) ? htmlentities($_GET['query'], ENT_QUOTES) : '' ?>">
@@ -95,26 +95,26 @@
                 </div>
             </div>
         </form>
-        <form class="form-inline mt-2 mt-sm-2">
+        <div class="d-flex mt-2">
             <div class="btn-group mr-2">
-                <a class="btn btn-sm-0 btn-outline-secondary" href="winkelmand.php"><i
+                <a class="btn btn-outline-secondary" href="winkelmand.php"><i
                             class="fas fa-shopping-basket"></i> <?= $aantal_winkelmand ?></a>
             </div>
-            <?php if (isset($_SESSION['loggedin'])) { ?>
+            <?php if (isset($_SESSION['loggedin'])): ?>
                 <div class="btn-group mr-2">
-                    <a class="btn btn-sm-0 btn-outline-secondary" href="profiel.php"><i class="fas fa-user-circle"></i>
+                    <a class="btn btn-outline-secondary" href="profiel.php"><i class="fas fa-user-circle"></i>
                         Profiel</a>
                 </div>
                 <div class="btn-group mr-2">
-                    <a class="btn btn-sm-0 btn-outline-secondary" href="logout.php"><i class="fas fa-sign-out-alt"></i>
+                    <a class="btn btn-outline-secondary" href="logout.php"><i class="fas fa-sign-out-alt"></i>
                         Afmelden</a>
                 </div>
-            <?php } else { ?>
+            <?php else: ?>
                 <div class="btn-group mr-2">
-                    <a class="btn btn-sm-0 btn-outline-secondary" href="login.php"><i class="fas fa-sign-in-alt"></i>
+                    <a class="btn btn-outline-secondary" href="login.php"><i class="fas fa-sign-in-alt"></i>
                         Aanmelden</a>
                 </div>
-            <?php } ?>
-        </form>
+            <?php endif; ?>
+        </div>
     </div>
 </nav>
